da90f858f946b1600e29ae4b5378ff47eb96e589
Dennis Hickox <frozzel@me.com>
2025-01-23 13:43:11 -0500

    testing route to getBlog on server

--- src/api/test.js
+++ src/api/test.js
@@ -2,7 +2,7 @@ import client from "./client";
 
 export const testApi = async () => {
   try {
-    const response = await client.get("api/test/test");
+    const response = await client.get("/test/test");
     console.log("IN Api", response.data);
     return response.data;
   } catch (error) {
